f739f493cbc13187e516406eaf293b662c93c5ca
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2025-10-01 14:00:28 +0300

    hw "Циклы урок 2" complited

--- src/main/java/org/example/cycles.java
+++ src/main/java/org/example/cycles.java
@@ -5,70 +5,90 @@ public class cycles {
 
         // 1
         System.out.println("-----------1------------");
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
+        int mounth = 0;
+        int money = 0;
+        while (money < 2459000){
+            mounth++;
+            money += 15000;
+            System.out.println("Месяц " + mounth + ", сумма накоплений равна: " + money + " рублей.");
         }
 
         // 2
         System.out.println("-----------2------------");
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
+        int nummber = 1;
+        while (nummber <= 10){
+            System.out.print(nummber+" ");
+            nummber++;
         }
-
+        System.out.println();
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
         // 3
         System.out.println("-----------3------------");
-        for (int i = 0; i <= 17; i = i + 2) {
-            System.out.println(i);
+        int population = 12_000_000;
+        int fertilityPerYear = 9;
+        int fertility = population / 1000 * fertilityPerYear;
+        for (int i = 0; i < 10; i++) {
+            fertility = population / 1000 * fertilityPerYear;
+            population += fertility;
+            System.out.println("Год " + i + ", численность населения составляет "+ population);
         }
-
         // 4
         System.out.println("-----------4------------");
-        for (int i = -10; i <= 10; i++) {
-            System.out.println(i);
+        int total = 15000;
+        int mounth2 = 0;
+        while (total < 12000000){
+            total *= 1.07;
+            mounth2++;
+            System.out.println("Месяц " +mounth2+ ", накоплено " +total);
         }
 
         // 5
         System.out.println("-----------5------------");
-        for (int i = 1094; i <= 2096; i += 4) {
-            System.out.println(i + " год является високосным");
+        int total3 = 15000;
+        int mounth3 = 0;
+        while (total3 < 12000000){
+            total3 *= 1.07;
+            mounth3++;
+            if (mounth3 % 3 ==0){
+                System.out.println("Месяц " +mounth3+ ", накоплено " +total3);
+            }
         }
 
         // 6
         System.out.println("-----------6------------");
-        for (int i = 7; i <= 98; i+=7) {
-            System.out.println(i);
+        int total4 = 15000;
+        int mounth4 = 0;
+        while (mounth4 < 108){
+            total4 *= 1.07;
+            mounth4++;
+            if (mounth4 % 6 == 0){
+                System.out.println("Месяц " +mounth4+ ", накоплено " +total4);
+            }
         }
 
 
         // 7
         System.out.println("-----------7------------");
-        for (int i = 1; i <= 512; i*=2) {
-            System.out.println(i);
+        int firstFriday = 3; // первая пятница месяца выпадает на 3 число
+
+        for (int day = firstFriday; day <= 31; day += 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
         }
 
 
-        // 8
+            // 8
         System.out.println("-----------8------------");
-        for (int month = 1, total = 0; month <= 12; month++) {
-            total += 29000;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        int currentYear = 2024;
+        int startYear = currentYear - 200;
+        int endYear = currentYear + 100;
+
+        for (int year = 0; year <= endYear; year += 79) {
+            if (year >= startYear) {
+                System.out.println(year);
+            }
         }
-
-
-
-        // 9
-        System.out.println("----------9------------");
-        for (int month = 1; month <= 12; month++) {
-            double total = 29000 * month * Math.pow(1.01, month);
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + (int)total + " рублей");
-        }
-
-
-        // 10
-        System.out.println("-----------10------------");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("2*" + i + "=" + (2 * i));
-        }
-
     }
 }
