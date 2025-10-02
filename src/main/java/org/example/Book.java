8625331714cd6b218fd119a51b092931c03ec0b1
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2025-10-02 15:23:02 +0300

    hw "Методы объектов" complited

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -27,5 +29,22 @@ public class Book {
 
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
+    }
+
+    public String toString(){
+        return "Книга - " + this.name + ", Автор - " + author.toString() + ", " + this.yearOfPublication + " год.";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, author, yearOfPublication);
     }
 }
